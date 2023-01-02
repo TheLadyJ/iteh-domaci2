bfbe7d628c1ab72a20a82adcbec7639cf5e6b456
TheLadyJ <jovana.vida@gmail.com>
2023-01-02 16:13:42 +0100

    Dodate veze izmedju modela

--- domacaKuhinja/app/Models/Kategorija.php
+++ domacaKuhinja/app/Models/Kategorija.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Kategorija extends Model
 {
     use HasFactory;
+
+
+    public function recepts(){
+        return $this->hasMany(Recept::class);
+    }
 }
--- domacaKuhinja/app/Models/Recept.php
+++ domacaKuhinja/app/Models/Recept.php
@@ -10,4 +10,13 @@ class Recept extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+
+    public function kategorija(){
+        return $this->belongsTo(Kategorija::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
